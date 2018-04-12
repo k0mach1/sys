d2ff492311d21d8b9bf68745afd836f5c4c7580c
Kosuke Machida <matchbaske.3.27@gmail.com>
2018-04-12 22:10:33 +0900

    minor fix on main_ex4

--- lecture1/main_ex4.f90
+++ lecture1/main_ex4.f90
@@ -10,8 +10,8 @@ WRITE(6, *) 'c?'
 READ(5, *) c
 WRITE(6, *)
 
-WRITE(6, *) 'cc(1) and cc(2)?'
-READ(5, *) cc(1), cc(2)
+WRITE(6, *) 'cc?'
+READ(5, *) cc
 WRITE(6, *)
 
 WRITE(6, *) 'c = ', c
@@ -21,23 +21,17 @@ WRITE(6, '(A, 2X, A)') 'c = ', c
 WRITE(6, *)
 
 WRITE(6, *) 'cc(1)= ', cc(1), ', ', 'cc(2) = ', cc(2)
-WRITE(6, '( (A, I6, 2A, I6) )') 'cc(1) = ', cc(1), ', ', 'cc(2) = ', cc(2)
+WRITE(6, '(2A)') 'cc = ', cc
 WRITE(6, *)
 
-ccc(1) = cc(1) + cc(2)
-ccc(2) = cc(1) - cc(2)
-ccc(3) = cc(1) * cc(2)
-ccc(4) = cc(1) / cc(2)
+ccc = c//cc
 
-WRITE(6, '(4(A, I8, A) )') 'ccc(1) = ', ccc(1), ', ', 'ccc(2) = ', ccc(2), ', ', 'ccc(3) = ', ccc(3), ', ', 'ccc(4) = ', ccc(4)
+WRITE(6, '(2A)') 'ccc = ', ccc
 WRITE(6, *)
 
-ccc(1) = c * ( cc(1)+cc(2) )
-ccc(2) = c * cc(1) + cc(2)
-ccc(3) = c * ( cc(1) * cc(2) )
-ccc(4) = c * cc(1) * cc(2)
+ccc = TRIM(c)//TRIM(cc)
 
-WRITE(6, '(4(A, I8, A) )') 'ccc(1) = ', ccc(1), ', ', 'ccc(2) = ', ccc(2), ', ', 'ccc(3) = ', ccc(3), ', ', 'ccc(4) = ', ccc(4)
+WRITE(6, '(2A)') 'ccc = ', ccc
 WRITE(6, *)
 
 STOP
