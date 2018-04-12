ca7c6cab5b16bd3eda0212cf951b689226720ecf
Kosuke Machida <matchbaske.3.27@gmail.com>
2018-04-12 23:25:11 +0900

    add lecture1/summation_g

---
+++ lecture1/summation_g.f90
@@ -0,0 +1,19 @@
+SUBROUTINE summation_g(n, g, sum_g)
+
+  IMPLICIT NONE
+
+  INTEGER :: n
+  REAL(8) :: g(n)
+  REAL(8) :: sum_g
+
+  INTEGER :: i
+
+  sum_f = 0.0D0
+
+  DO i = 1, n
+    sum_g = sum_g+ g(i)
+  END DO
+
+  RETURN
+
+END SUBROUTINE summation_g
