84e2effef4e51c9b21ed76c7e7f6bdc1f7a44f84
Kosuke Machida <matchbaske.3.27@gmail.com>
2018-04-12 23:54:18 +0900

    add lecture1/main_ex9

---
+++ lecture1/main_ex9.f90
@@ -0,0 +1,52 @@
+!theme: 四面体の体積を算出するプログラム
+PROGRAM main_ex9
+
+IMPLICIT NONE
+
+REAL(8) :: x(4), y(4), z(4)
+REAL(8) :: volume
+REAL(8) :: ax, ay, az
+REAL(8) :: bx, by, bz
+REAL(8) :: cx, cy, cz
+REAL(8) :: dx, dy, dz
+CHARACTER :: dataname
+
+OPEN(10, FILE = 'input.dat')
+READ(10, *) dataname
+READ(10, *) x(1), y(1), z(1)
+READ(10, *) dataname
+READ(10, *) x(2), y(2), z(2)
+READ(10, *) dataname
+READ(10, *) x(3), y(3), z(3)
+READ(10, *) dataname
+READ(10, *) x(4), y(4), z(4)
+CLOSE(10)
+
+ax = x(2) - x(1)
+ay = y(2) - y(1)
+ax = z(2) - z(1)
+
+bx = x(3) - x(1)
+by = y(3) - y(1)
+bx = z(3) - z(1)
+
+cx = x(4) - x(1)
+cy = y(4) - y(1)
+cx = z(4) - z(1)
+
+dx = ay * bz - az * by
+dy = az * bx - ax * bz
+dx = ax * by - ay * bx
+
+volume = ( 1.0D0 / 6.0D0 ) * ( cx * dx + cy * dy + cz * dz )
+
+WRITE(6, '(A, F0.8)') 'volume = ', volume
+WRITE(6, *)
+
+OPEN(90, FILE = 'output.dat')
+WRITE(90, '(A, F0.8)') 'volume = ', volume
+CLOSE(90)
+
+STOP
+
+END PROGRAM main_ex9
