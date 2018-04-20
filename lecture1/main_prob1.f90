7ae518d4931ec22c351efc55a827191a0c098080
Kosuke Machida <matchbaske.3.27@gmail.com>
2018-04-20 17:51:13 +0900

    minor fix on main_prob1

--- lecture1/main_prob1.f90
+++ lecture1/main_prob1.f90
@@ -3,7 +3,7 @@ PROGRAM main_prob1
 IMPLICIT NONE
 
 Integer :: info, lda, ldb, n, i
-REAL, Allocatable :: a(:, :), b(:)
+REAL(8), Allocatable :: a(:, :), b(:)
 Integer, Allocatable :: ipiv(:)
 CHARACTER :: dataname
 
@@ -16,17 +16,17 @@ ALLOCATE(a(lda, n), b(ldb), ipiv(n))
 
 OPEN(10, FILE = 'input2.dat')
 READ(10, *) dataname
-READ(10, *) a(0, 0), a(0,1), a(0,2), a(0,3), a(0,4)
+READ(10, *) a(1,1), a(1,2), a(1,3), a(1,4), a(1,5)
 READ(10, *) dataname
-READ(10, *) a(1,0), a(1,1), a(1,2), a(1,3), a(1,4)
+READ(10, *) a(2,1), a(2,2), a(2,3), a(2,4), a(2,5)
 READ(10, *) dataname
-READ(10, *) a(2,0), a(2,1), a(2,2), a(2,3), a(2,4)
+READ(10, *) a(3,1), a(3,2), a(3,3), a(3,4), a(3,5)
 READ(10, *) dataname
-READ(10, *) a(3,0), a(3,1), a(3,2), a(3,3), a(3,4)
+READ(10, *) a(4,1), a(4,2), a(4,3), a(4,4), a(4,5)
 READ(10, *) dataname
-READ(10, *) a(4,0), a(4,1), a(4,2), a(4,3), a(4,4)
+READ(10, *) a(5,1), a(5,2), a(5,3), a(5,4), a(5,5)
 READ(10, *) dataname
-READ(10, *) b(0), b(1), b(2), b(3), b(4)
+READ(10, *) b(1), b(2), b(3), b(4), b(5)
 CLOSE(10)
 
 CALL dgesv(n, 1, a, lda, ipiv, b, ldb, info)
