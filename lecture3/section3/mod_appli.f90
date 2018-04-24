d1db50ceff3ea22c554ab3eb5fba769a31aaa9e8
Kosuke Machida <matchbaske.3.27@gmail.com>
2018-04-24 11:23:29 +0900

    fix mod_appli in lecture3/section3

--- lecture3/section3/mod_appli.f90
+++ lecture3/section3/mod_appli.f90
@@ -4,6 +4,7 @@ MODULE mod_appli
 USE mod_nodes3d
 USE mod_localelement3d
 USE mod_elements3d
+USE mod_rectmesher3d
 
 !--------------------------------------------------------------------
 
@@ -14,6 +15,7 @@ IMPLICIT NONE
 TYPE(struct_nodes3d), POINTER        :: ns3d
 TYPE(struct_localelement3d), POINTER :: le3d
 TYPE(struct_elements3d), POINTER     :: es3d
+TYPE(struct_rectmesher3d), POINTER   :: rm3d
 
 !--------------------------------------------------------------------
 
@@ -28,9 +30,10 @@ SUBROUTINE start_appli()
   INTEGER :: le3d_nboundaries
   INTEGER :: le3d_nnodes
   INTEGER :: es3d_n
-  INTEGER, ALLOCATABLE :: es3d_connectivity(:, :)
+  INTEGER :: rm3d_n_x(3)
 
-  REAL(8), ALLOCATABLE :: ns3d_x(:, :)
+  REAL(8) :: rm3d_x_start(3)
+  REAL(8) :: rm3d_x_end(3)
 
   CHARACTER(1) :: dataname
 
@@ -39,15 +42,32 @@ SUBROUTINE start_appli()
   ALLOCATE( ns3d )
   ALLOCATE( le3d )
   ALLOCATE( es3d )
+  ALLOCATE( rm3d )
 
 !--------------------------------------------------------------------
 
-  ns3d_n = 8
-  le3d_nboundaries = 6
-  le3d_nnodes = 8
-  es3d_n = 1
+  OPEN(13, FILE = 'param_meshing.dat')
+
+  READ(13, *) dataname
+  READ(13, *) rm3d_n_x(1), rm3d_n_x(2), rm3d_n_x(3)
+  READ(13, *) dataname
+  READ(13, *) rm3d_x_start(1), rm3d_x_start(2), rm3d_x_start(3)
+  READ(13, *) dataname
+  READ(13, *) rm3d_x_end(1), rm3d_x_end(2), rm3d_x_end(3)
+  READ(13, *) dataname
+
+  CLOSE(13)
 
 !--------------------------------------------------------------------
+
+  CALL get_nodes3d_n(ns3d, ns3d_n)
+
+  CALL get_localelement3d_nboundaries(le3d, le3d_nboundaries)
+  CALL get_localelement3d_nnodes(le3d, le3d_nnodes)
+
+  CALL get_elements3d_n(es3d, es3d_n)
+
+!--------------------------------------------------------------
 
   CALL init_nodes3d(ns3d, ns3d_n)
 
@@ -55,63 +75,7 @@ SUBROUTINE start_appli()
 
   CALL init_elements3d(es3d, ns3d, le3d, es3d_n)
 
-!--------------------------------------------------------------------
-
-  ALLOCATE( ns3d_x(3, ns3d_n) )
-
-  ! Node 1
-  ns3d_x(1, 1) = 0.0D0
-  ns3d_x(2, 1) = 0.0D0
-  ns3d_x(3, 1) = 0.0D0
-  ! Node 2
-  ns3d_x(1, 2) = 1.0D0
-  ns3d_x(2, 2) = 0.0D0
-  ns3d_x(3, 2) = 0.0D0
-  ! Node 3
-  ns3d_x(1, 3) = 0.0D0
-  ns3d_x(2, 3) = 1.0D0
-  ns3d_x(3, 3) = 0.0D0
-  ! Node 4
-  ns3d_x(1, 4) = 1.0D0
-  ns3d_x(2, 4) = 1.0D0
-  ns3d_x(3, 4) = 0.0D0
-  ! Node 5
-  ns3d_x(1, 5) = 0.0D0
-  ns3d_x(2, 5) = 0.0D0
-  ns3d_x(3, 5) = 1.0D0
-  ! Node 6
-  ns3d_x(1, 6) = 1.0D0
-  ns3d_x(2, 6) = 0.0D0
-  ns3d_x(3, 6) = 1.0D0
-  ! Node 7
-  ns3d_x(1, 7) = 0.0D0
-  ns3d_x(2, 7) = 1.0D0
-  ns3d_x(3, 7) = 1.0D0
-  ! Node 8
-  ns3d_x(1, 8) = 1.0D0
-  ns3d_x(2, 8) = 1.0D0
-  ns3d_x(3, 8) = 1.0D0
-
-  CALL set_nodes3d_x(ns3d, ns3d_x)
-
-  ALLOCATE( es3d_connectivity(le3d_nnodes, es3d_n) )
-
-  ! Element 1
-  es3d_connectivity(1, 1) = 1
-  es3d_connectivity(2, 1) = 2
-  es3d_connectivity(3, 1) = 4
-  es3d_connectivity(4, 1) = 3
-  es3d_connectivity(5, 1) = 5
-  es3d_connectivity(6, 1) = 6
-  es3d_connectivity(7, 1) = 8
-  es3d_connectivity(8, 1) = 7
-
-  CALL set_elements3d_connectivity(es3d, es3d_connectivity)
-
-!--------------------------------------------------------------------
-
-  DEALLOCATE( ns3d_x )
-  DEALLOCATE( es3d_connectivity )
+  CALL init_rectmesher3d(rm3d, ns3d, le3d, es3d, rm3d_n_x, rm3d_x_start, rm3d_x_end)
 
 !--------------------------------------------------------------------
 
@@ -120,6 +84,7 @@ SUBROUTINE start_appli()
 !####################################################################
 END SUBROUTINE start_appli
 !####################################################################
+
 
 ! Run appli
 !####################################################################
@@ -148,6 +113,10 @@ SUBROUTINE run_appli()
 
 !--------------------------------------------------------------------
 
+  CALL cal_rectmesher3d(rm3d)
+
+!--------------------------------------------------------------------
+
   CALL get_nodes3d_n(ns3d, ns3d_n)
 
   ALLOCATE( ns3d_x(3, ns3d_n) )
@@ -168,41 +137,67 @@ SUBROUTINE run_appli()
 
 !--------------------------------------------------------------------
 
+  OPEN(10, FILE = 'mesh.dat')
+  WRITE(10, '(A)') '!NODE'
+  DO id = 1, ns3d_n
+  WRITE( 10,'( I8, 3(A, E17.8) )' ) id, ( ',', ns3d_x(i, id), i = 1, 3  )
+
+!--------------------------------------------------------------------
+
+  OPEN(11, FILE = 'ic.dat')
+
+  WRITE(11, '(A)') '!DISPLACEMENT'
+
+  DO id = 1, ns3d_n
+
+  WRITE( 11, '(I8, 3(A, E17.8) )' ) id, ( ', ', ns3d_u( 3*(id-1)+i ), i = 1, 3 )
+
+  END DO
+
+  WRITE(11, '(A)') '!END'
+
+  CLOSE(11)
+  END DO
+             ', ', le3d_nnodes, ', ', 2
+  DO ie = 1, es3d_n
+        na = 1, le3d_nnodes )
+  END DO
+  WRITE(10,'(A)') '!END'
+  CLOSE(10)
+  WRITE( 10, '(A, 3(A, I3) )' ) '!ELEMENT', ', ', le3d_nboundaries, WRITE( 10, '( I8, 27(A, I8) )' ) ie, ( ',', es3d_connectivity(na, ie))
+
+!--------------------------------------------------------------------
+
+  OPEN(12, FILE = 'bc.dat')
+
+  WRITE(12, '(A)') '!DISPLACEMENT'
+
+  DO id = 1, ns3d_n
+
+  WRITE( 12, '(I8, 3(A, I8) )' ) id, ( ', ', ns3d_bc( 3*(id-1)+i ), i = 1, 3 )
+
+  END DO
+
+  WRITE(12, '(A)') '!END'
+
+  CLOSE(12)
+
+!--------------------------------------------------------------------
+
   OPEN(14, FILE = 'mesh.inp')
 
   WRITE(14, '( 5(I8, 1X) )') ns3d_n, es3d_n, 3, 13, 0
 
   DO id = 1, ns3d_n
 
-    WRITE( 14, '( (I8, 1X), 3(E17.8, 1X) )' ) id, ( ns3d_x(i, id), i = 1, 3 )
+  WRITE( 14, '( (I8, 1X), 3(E17.8, 1X) )' ) &
+        id, ( ns3d_x(i, id), i = 1, 3 )
 
   END DO
 
   DO ie = 1, es3d_n
 
-    WRITE( 14, '( 2(I8, 1X), (A5, 1X), 27(I8, 1X) )' ) ie, 1, '  hex', ( es3d_connectivity(na, ie), na = 1, le3d_nnodes )
-
-  END DO
-
-  WRITE(14, '( 4(I8, 1X) )') 1, 3
-  WRITE(14, '(A)') 'DISPLACEMENT, m'
-
-  DO id = 1, ns3d_n
-
-    WRITE( 14, '( (I8, 1X), 3(E17.8, 1X) )' ) id, ( ns3d_u( 3*(id-1)+i ), i = 1, 3 )
-
-  END DO
-
-  WRITE(14, '( 14I8 )') 1, 1
-  WRITE(14, '( (A, 1X) )') 'VOLUME, m3'
-
-  DO ie = 1, es3d_n
-
-    WRITE( 14, '( (I8, 1X), (E17.8, 1X) )' ) ie, es3d_volume(ie)
-
-  END DO
-
-  CLOSE(14)
+  WRITE( 14, '( 2(I8, 1X), (A5, 1X), 27(I8, 1X) )' ) ie, 1, '  hex', ( es3d_connectivity(na, ie), na = 1, le3d_nnodes )
 
 !--------------------------------------------------------------------
 
@@ -215,6 +210,18 @@ SUBROUTINE run_appli()
 !--------------------------------------------------------------------
 
   RETURN
+  END DO
+  DO id = 1, ns3d_n
+  END DO
+  DO ie = 1, es3d_n
+  END DO
+  CLOSE(14)
+  WRITE(14, '( 4(I8, 1X) )') 1, 3
+  WRITE(14, '(A)') 'DISPLACEMENT, m'
+  WRITE( 14, '( (I8, 1X), 3(E17.8, 1X) )' ) id, ( ns3d_u( 3*(id-1)+i ), i = 1, 3 )
+  WRITE(14, '( 14I8 )') 1, 1
+  WRITE(14, '( (A, 1X) )') 'VOLUME, m3'
+  WRITE( 14, '( (I8, 1X), (E17.8, 1X) )' ) ie, es3d_volume(ie)
 
 !####################################################################
 END SUBROUTINE run_appli
@@ -228,12 +235,14 @@ SUBROUTINE finish_appli()
   CALL del_nodes3d(ns3d)
   CALL del_localelement3d(le3d)
   CALL del_elements3d(es3d)
+  CALL del_rectmesher3d(rm3d)
 
 !--------------------------------------------------------------------
 
   DEALLOCATE( ns3d )
   DEALLOCATE( le3d )
   DEALLOCATE( es3d )
+  DEALLOCATE( rm3d )
 
 !--------------------------------------------------------------------
 
